b03230a2d16a997b3a8e98c1ba43292e1d2d2df5
Wes Cravens <wcravens@cortex-it.com>
2016-01-22 08:06:12 -0600

    Add some module patterns from Reliable Javascript, Chapt 3

--- test/module_pattern.js
+++ test/module_pattern.js
@@ -14,6 +14,20 @@ describe( 'The Revealing Module Pattern', function() {
 // http://bites.goodeggs.com/posts/export-this/
 
 describe( 'Namespace: Exporting an object', function() {
+
+  // Reliable Javascript, Chapt 3
+  describe( 'Modules-at-Will', function() {
+    it( 'provides a function on a namespace' );
+    it( 'the init function returns an object holding the API' );
+    it( 'the init function can be used to inject dependencies' ); 
+  } );
+
+  // Reliable Javascript, Chapt 3
+  describe( 'Immediately-Executing Modules', function() {
+    it( 'is setup immediately when invoked/loaded' );
+    it( 'is often used for a Singleton pattern' );
+    it( 'requires that any injectable dependencies are available when it is invoked' );
+  } );
 } );
 
 
